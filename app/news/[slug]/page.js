10ef9c85ec1871dbc8912113ff30ff4daba9e2f2
Denis <136696817+Ghizma@users.noreply.github.com>
2026-06-12 12:59:09 +0300

    Nested Routes Inside Dynamic Routes

--- app/news/[slug]/page.js
+++ app/news/[slug]/page.js
@@ -1,23 +1,26 @@
 import { DUMMY_NEWS } from "@/dummy-news";
-import { redirect } from "next/dist/server/api-utils";
+import Link from "next/link";
 import { notFound } from "next/navigation";
 
 export default function NewsDetailPage({ params }) {
   const newsSlug = params.slug;
-  const newsItems = DUMMY_NEWS.find((newsItems) => newsItems.slug === newsSlug);
+  const newsItem = DUMMY_NEWS.find((newsItem) => newsItem.slug === newsSlug);
 
-  if (!newsItems) {
+  if (!newsItem) {
     notFound();
   }
 
   return (
     <article className="news-article">
       <header>
-        <img src={`/images/news/${newsItems.image}`} alt={newsItems.title} />
-        <h1>{newsItems.title}</h1>
-        <time dateTime={newsItems.date}>{newsItems.date} </time>
+        <Link href={`/news/${newsItem.slug}/image`}>
+          <img src={`/images/news/${newsItem.image}`} alt={newsItem.title} />
+        </Link>
+
+        <h1>{newsItem.title}</h1>
+        <time dateTime={newsItem.date}>{newsItem.date} </time>
       </header>
-      <p>{newsItems.content}</p>
+      <p>{newsItem.content}</p>
     </article>
   );
 }
